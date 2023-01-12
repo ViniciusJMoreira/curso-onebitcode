dbda2b65b9ad5efb0408375233274cec9a786d3a
Vinicius <viniciusit.moreira@gmail.com>
2023-01-12 14:21:15 +0100

    update

--- modulo-js/base/array/pratice-3.js
+++ modulo-js/base/array/pratice-3.js
@@ -45,16 +45,48 @@ arr.forEach(exibirElementos)
 const isArray = Array.isArray(arr)
 console.log(isArray)
 
+
+// Metodo map cria um array chamando uma função para cada elemento do array
+// Não altera o array original
 const persons = [
-  {firstname : "Malcom", lastname: "Reynolds"},
-  {firstname : "Kaylee", lastname: "Frye"},
-  {firstname : "Jayne", lastname: "Cobb"}
+  {firstname : "Vinicius", lastname: "Moreira"},
+  {firstname : "Moritz", lastname: "Moreira"},
+  {firstname : "Ericksen", lastname: "Moreira"}
 ];
 
-const result = persons.map(getFullName);
+const map = persons.map(getFullName);
 
 function getFullName(item) {
   return [item.firstname,item.lastname].join(" ");
 }
 
-console.log(result)
+console.log(map)
+
+// Metodo reduce
+const numbers = [1, 2, 3];
+const reduce = numbers.reduce(function(valorAcumulado, valorAtual){
+  return valorAcumulado + valorAtual
+});
+
+console.log(reduce)
+
+const personagens = [
+  {nivel: 42 , nome: 'Ira', raca: 'Orc', classe: 'Xamã'},
+  {nivel: 28 , nome: 'Garrosh', raca: 'Orc', classe: 'Guerreiro'},
+  {nivel: 35 , nome: 'Varok', raca: 'Orc', classe: 'Guerreiro'},
+  {nivel: 35 , nome: 'Uther', raca: 'Humano', classe: 'PaladHumano'},
+  {nivel: 26 , nome: 'Jaina', raca: 'Humano', classe: 'Maga'},
+  {nivel: 39 , nome: 'Tynande', raca: 'Elfo Noturno', classe: 'Sacerdotista'},
+  {nivel: 42 , nome: 'Muradin', raca: 'Anão', classe: 'Guerreiro'},
+]
+
+const racas = personagens.reduce(function(valorAcumulado,personagem){
+  if (valorAcumulado[personagem.classe]){
+    valorAcumulado[personagem.classe].push(personagem)
+  }else {
+    valorAcumulado[personagem.classe] = [personagem]
+  }
+  return valorAcumulado
+}, {})
+
+console.log(racas)
